c428e8c20d3a0823d00aee3692ffbaaf5377b856
алый <iseurie@gmail.com>
2019-12-10 08:19:45 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/kavorite/dcpfp
+
+go 1.13
+
+require (
+	github.com/bwmarrin/discordgo v0.20.1
+	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
+	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
+	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
+	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
+	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
+	github.com/jbowles/disfun v0.0.0-20160912183503-62faa31f9b2c
+	github.com/juju/clock v0.0.0-20190205081909-9c5c9712527c // indirect
+	github.com/juju/errors v0.0.0-20190930114154-d42613fe1ab9 // indirect
+	github.com/juju/loggo v0.0.0-20190526231331-6e530bcce5d8 // indirect
+	github.com/juju/utils v0.0.0-20180820210520-bf9cc5bdd62d // indirect
+	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	gopkg.in/juju/names.v2 v2.0.0-20190813004204-e057c73bd1be // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+)
---
+++ mod.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+    "sort"
+
+    dgo "github.com/bwmarrin/discordgo"
+    "github.com/pkg/browser"
+    "github.com/jbowles/disfun"
+)
+
+func dlev(a, b string) int {
+    return disfun.DamerauLevenshtein(a, b)
+}
+
+func main() {
+    uid := flag.String("t", "", "Target user snowflake")
+    tag := flag.String("g", "", "Target user tag")
+    token := flag.String("T", "", "Authentication token")
+    prn := flag.Bool("p", false, "Print-only")
+    flag.Parse()
+    if *tag == "" && *uid == "" {
+        fmt.Fprintf(os.Stderr, "fatal: no target provided\n")
+        os.Exit(1)
+    }
+    if *token == "" {
+        *token = os.Getenv("DCPFP_TOKEN")
+    }
+    if *token == "" {
+        fmt.Fprintf(os.Stderr, "fatal: no token provided\n")
+        os.Exit(2)
+    }
+    client, err := dgo.New(*token)
+    if *tag != "" {
+        // emplace uid if none given
+        relations, err := client.RelationshipsGet()
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "fatal: retrieve relations: %s\n", err)
+            os.Exit(3)
+        }
+        if len(relations) == 0 {
+            fmt.Fprintf(os.Stderr, "fatal: you have no friends >:(\n")
+            os.Exit(4)
+        }
+        // find the Discord nick#tag with the closest Damerau—Levenshtein
+        // distance to the search term
+        levs := make(map[*dgo.Relationship]int, len(relations))
+        for i, r := range relations {
+            uname := relations[i].User.Username
+            discriminator := relations[i].User.Discriminator
+            ltag := fmt.Sprintf("%s#%s", uname, discriminator)
+            levs[r] = dlev(*tag, ltag)
+        }
+        sort.Slice(relations, func(i, j int) bool {
+            return levs[relations[i]] < levs[relations[j]]
+        })
+        *uid = relations[0].User.ID
+    }
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "fatal: login failure: %s\n", err)
+        os.Exit(4)
+    }
+    u, err := client.User(*uid)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "fatal: retrieve user: %s\n", err)
+        os.Exit(4)
+    }
+    uri := fmt.Sprintf("https://cdn.discordapp.com/avatars/%s/%s.jpg?size=2048",
+        u.ID, u.Avatar)
+    if *prn {
+        fmt.Println(uri)
+    } else {
+        err := browser.OpenURL(uri)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "failure spawning browser: %s\n", err)
+            os.Exit(5)
+        }
+    }
+}
